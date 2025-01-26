70b71a5003ba3c3b89deca802f093df87762ecb9
GabrielFLorentino <gabrieledufloretnino@hotmail.com>
2025-01-26 12:09:45 -0300

    Revert "Find game by id"
    This reverts commit 761ad14c7adaf7cac752f70571828f662c00f412.

--- src/main/java/com/gabrielflorentino/dslist/controllers/GameController.java
+++ src/main/java/com/gabrielflorentino/dslist/controllers/GameController.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gabrielflorentino.dslist.dto.GameDTO;
 import com.gabrielflorentino.dslist.dto.GameMinDTO;
 import com.gabrielflorentino.dslist.services.GameService;
 
@@ -17,12 +15,6 @@ import com.gabrielflorentino.dslist.services.GameService;
 public class GameController {
 	@Autowired
 	private GameService gameService;	
-	
-	@GetMapping(value = "/{id}")
-	public GameDTO findById(@PathVariable Long id) {
-		GameDTO result = gameService.findById(id);
-		return result;
-	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
